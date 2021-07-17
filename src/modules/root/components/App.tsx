c1de15903b82109912ff854937fd886b671216f3
toaderflorin <toaderflorin@gmail.com>
2021-07-17 15:12:59 +0300

    Moving components to different folders (renamed module names).

--- src/modules/root/components/App.tsx
+++ src/modules/root/components/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   )
 }
---
+++ src/ui-kit/atoms/ViewFill.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function ViewFill(props: any) {
+  const { children } = props
+
+  return (
+    <View>
+
+    </View>
+  )
+}
+
+export default ViewFill
